690b2d3ab4a548dfdff241f5c5e2d92878fa26f4
Leandro <vitto.leandro@gmail.com>
2021-05-03 22:37:19 +0200

    Make Payment

--- api/app/Http/Controllers/Api/Orders/OrderController.php
+++ api/app/Http/Controllers/Api/Orders/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Orders;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 
 class OrderController extends Controller
 {
@@ -19,7 +20,28 @@ class OrderController extends Controller
         return response()->json($data,200);
     }
 
-    public function makePayment(Request $request,Gateway $gateway){
-        return 'make payment';
+    public function makePayment(OrderRequest $request,Gateway $gateway){
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if($result->success){
+            $data = [
+                'success' => true,
+                'message' => "Transazione eseguita con Successo!"
+            ];
+            return response()->json($data,200);
+        }else{
+            $data = [
+                'success' => false,
+                'message' => "Transazione Fallita!!"
+            ];
+            return response()->json($data,401);
+        }
     }
 }
