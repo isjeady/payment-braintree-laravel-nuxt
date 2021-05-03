91fde1a458adc45b08f6d9f5343f1ad47079b252
Leandro <vitto.leandro@gmail.com>
2021-05-03 23:41:25 +0200

    Payment Frontend

--- app/pages/checkout/_id.vue
+++ app/pages/checkout/_id.vue
@@ -3,8 +3,14 @@
     <div class="text-2xl">
       Stai acquistando il prodotto con id: {{ $route.params.id }}
     </div>
-
-    <Payment />
+    {{ form }}
+    <Payment
+      ref="paymentRef"
+      :authorization="tokenApi"
+      @loading="handleLoading"
+      @onSuccess="paymentOnSuccess"
+      @onError="paymentOnError"
+    />
 
     <button
       v-if="!disableBuyButton"
@@ -26,10 +32,59 @@
 
 <script>
 export default {
+  async asyncData ({ app }) {
+    let tokenApi = null
+    const response = await app.$axios.$get('/api/orders/generate')
+    tokenApi = response.token
+
+    return {
+      tokenApi,
+      loadingPayment: false
+    }
+  },
   data () {
     return {
-      disableBuyButton: false,
-      loadingPayment: true
+      tokenApi: '',
+      disableBuyButton: true,
+      loadingPayment: true,
+      form: {
+        token: '',
+        product: ''
+      }
+    }
+  },
+  mounted () {
+    this.form.product = this.$route.params.id
+  },
+  methods: {
+    handleLoading () {
+      this.disableBuyButton = false
+    },
+    paymentOnSuccess (nonce) {
+      // alert(nonce);
+      this.form.token = nonce
+      this.buy()
+    },
+    // eslint-disable-next-line node/handle-callback-err
+    paymentOnError (error) {
+
+    },
+    beforeBuy () {
+      this.$refs.paymentRef.$refs.paymentBtnRef.click()
+    },
+    async buy () {
+      this.disableBuyButton = true
+      this.loadingPayment = true
+
+      try {
+        await this.$axios.$post('/api/orders/make/payment', { ...this.form })
+        // const message = response.message
+        // alert(message)
+        this.$router.push({ path: '/checkout/thankyou' })
+      } catch (error) {
+        this.disableBuyButton = false
+        this.loadingPayment = false
+      }
     }
   }
 }
---
+++ app/pages/checkout/thankyou.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col mx-auto  max-w-lg p-10 justify-middle">
+    <div class="text-4xl">
+      Grazie per aver acquistato!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
